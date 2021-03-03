da361b9a62874c41062839ed9c1a07aa331307ab
kiranlakhani20 <55124757+kiranlakhani20@users.noreply.github.com>
2021-03-03 12:36:54 +0530

    6001:randfile_6001

--- commits2.sh
+++ commits2.sh
@@ -1,7 +1,7 @@
 string=""
 x=1
-j=9000
-while [ $x -le 220 ]
+j=6000
+while [ $x -le 300 ]
 do
  string="touch randfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
  echo $string
