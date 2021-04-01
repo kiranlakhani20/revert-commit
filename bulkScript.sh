400274b524c4a0722a02b16590ccb66b4f35d4ab
kiranlakhani20 <55124757+kiranlakhani20@users.noreply.github.com>
2021-04-01 23:26:19 +0530

    800001:randfile_800001

--- bulkScript.sh
+++ bulkScript.sh
@@ -1,7 +1,7 @@
 string=""
 x=1
-j=500000
-while [ $x -le 1000 ]
+j=800000
+while [ $x -le 999 ]
 do
  string="touch randfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
  echo $string
