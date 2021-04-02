b67aaf5da9fae3962de29d48a66f8dcab1702bd5
kiranlakhani20 <55124757+kiranlakhani20@users.noreply.github.com>
2021-04-02 16:53:59 +0530

    47201:randfile_47201

---
+++ bulkScript1.sh
@@ -0,0 +1,12 @@
+string=""
+x=1
+j=47200
+while [ $x -le 1000 ]
+do
+ string="touch randfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
+ echo $string
+ eval "$string"
+ x=$[$x+1]
+done
+echo $push
+eval "$push"
