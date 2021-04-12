d237c28ff63e5aa8fb07830fe3c7c56f895544e6
kiranlakhani20 <55124757+kiranlakhani20@users.noreply.github.com>
2021-04-12 21:17:38 +0530

    91001:randfile_91001

--- pushBulkCommits.sh
+++ pushBulkCommits.sh
@@ -1,7 +1,7 @@
 string=""
 x=1
-j=90000
-while [ $x -le 500 ]
+j=91000
+while [ $x -le 700 ]
 do
  string="touch randfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
  echo $string
